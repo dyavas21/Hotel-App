e28c08d056449e1b6bd642791a18ffbae26b4a9d
ROG <dyavas21@gmail.com>
2022-01-30 23:40:57 +0700

    appbar 2

--- hotelapp/lib/pages/home_page.dart
+++ hotelapp/lib/pages/home_page.dart
@@ -43,35 +43,117 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      appBar: PreferredSize(
-        preferredSize: Size(double.infinity, 125),
-        child: AppBar(
-          backgroundColor: Color(0xff5589F0),
-          elevation: 0,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // appBar: PreferredSize(
+      //   preferredSize: Size(double.infinity, 125),
+      //   child: AppBar(
+      //     backgroundColor: Color(0xff5589F0),
+      //     elevation: 0,
+      //     title: Row(
+      //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //       children: [
+      //         Image.asset(
+      //           'assets/nav1.png',
+      //           width: 26,
+      //         ),
+      //         Text(
+      //           'Discover',
+      //           style: GoogleFonts.poppins(
+      //             fontSize: 20,
+      //             fontWeight: FontWeight.w600,
+      //           ),
+      //         ),
+      //         Image.asset(
+      //           'assets/notification.png',
+      //           width: 26,
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body: ListView(
+        children: [
+          Stack(
             children: [
-              Image.asset(
-                'assets/nav1.png',
-                width: 26,
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    height: 125,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color(0xff5589F0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset(
+                          'assets/nav1.png',
+                          width: 26,
+                        ),
+                        Text(
+                          'Discover',
+                          style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Image.asset(
+                          'assets/notification.png',
+                          width: 26,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                'Discover',
-                style: GoogleFonts.poppins(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                margin: EdgeInsets.only(top: 90, left: 24, right: 24),
+                height: 80,
+                width: MediaQuery.of(context).size.width - (2 * 24),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Your Balance',
+                          style: GoogleFonts.poppins(
+                            color: Color(0xffB7B7B7),
+                          ),
+                        ),
+                        Text(
+                          'IDR 9.200.301',
+                          style: GoogleFonts.poppins(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xff4E82EA),
+                          ),
+                        )
+                      ],
+                    ),
+                    Image.asset(
+                      'assets/plus.png',
+                      width: 24,
+                    ),
+                  ],
                 ),
               ),
-              Image.asset(
-                'assets/notification.png',
-                width: 26,
-              ),
+              Row(
+                children: [
+                  Text('Recommended Hotels'),
+                  Text('View all'),
+                ],
+              )
             ],
           ),
-        ),
-      ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 24),
+        ],
       ),
     );
   }
